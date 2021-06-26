4a1025515660b7d4b58c769da709acf30f276521
alex-s-nik <to_ctrl@mail.ru>
2021-06-26 22:36:24 +0300

    feat: mistakes count

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-//const text = "This is a text!\nThis is a text!";
+const text = "This is a text!\nThis is a text!";
 //const text = "asdf";
-
+/*
 const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dapibus placerat mattis. Duis placerat ligula vitae dolor egestas consectetur. Aenean vel odio non massa malesuada lobortis. Sed aliquam enim sit amet urna vehicula euismod. Aliquam at velit accumsan, scelerisque eros ac, vestibulum dui. Quisque a sem vitae erat ultrices venenatis. Sed vel efficitur nibh. Maecenas vitae dapibus neque, vel blandit velit. Proin nibh orci, fermentum id cursus vel, consectetur id neque. Nam leo lectus, accumsan id molestie sit amet, viverra eu mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam rhoncus justo eget dictum rutrum. Etiam sit amet auctor elit.
 Vestibulum porta sollicitudin est id condimentum. Donec purus erat, finibus id egestas ut, facilisis et turpis. Phasellus pellentesque, enim et rutrum condimentum, erat metus vehicula quam, convallis sollicitudin libero purus congue odio. Aliquam vel eros ligula. Praesent quis eros id nisi vehicula mollis. Vivamus at blandit arcu. Pellentesque euismod eu quam nec ullamcorper. Integer volutpat semper massa, quis tincidunt diam. Integer ut rhoncus augue. Curabitur eu porttitor felis, sit amet feugiat risus. Suspendisse at ipsum consequat, convallis metus vel, convallis dolor. Donec eu magna nec ante eleifend condimentum ac in turpis. In porta convallis aliquet.
 Pellentesque sodales augue sit amet malesuada varius. Integer molestie ante sed tortor tincidunt, eget scelerisque erat aliquam. Quisque convallis lobortis diam. Suspendisse efficitur molestie ipsum vel hendrerit. Sed vehicula, quam eu varius aliquam, massa ligula malesuada est, nec commodo ipsum ex sit amet eros. Aliquam laoreet, justo non fermentum vestibulum, risus lorem viverra urna, in laoreet risus nunc in nisl. Maecenas id dictum neque, non iaculis diam.
 Aenean ac egestas eros, sed scelerisque nisi. Nulla felis elit, convallis in erat et, ultrices sollicitudin nisl. Integer in lacus a risus fringilla tempor. Pellentesque libero lacus, pretium eget urna hendrerit, porttitor tristique arcu. Quisque quis lacinia enim. Integer convallis dui a ipsum semper, eget scelerisque diam convallis. Nullam tempor tortor ac varius tincidunt. Vivamus sed lectus dui. Praesent porta felis non feugiat vestibulum. Proin pulvinar, tellus auctor viverra pellentesque, ex mauris pharetra quam, nec hendrerit sem est et ipsum. Pellentesque ultrices eu purus ac aliquet. In pretium vestibulum urna, interdum sodales lorem sodales congue. Maecenas euismod laoreet purus. Sed sit amet diam a ipsum blandit posuere id at magna. Ut volutpat finibus nunc, vitae luctus augue sagittis eget. Morbi varius finibus felis, a sollicitudin ipsum mattis convallis.
 Donec lectus sem, sodales consequat nunc id, bibendum pretium nisi. Morbi quis lobortis nisl. Phasellus et neque non massa tempus tempor in id tellus. Morbi facilisis, ligula ac consectetur ullamcorper, turpis lacus cursus diam, sodales ultrices velit purus ac felis. Praesent convallis volutpat diam eget facilisis. Nunc eleifend est id sem congue, quis tempor ligula condimentum. Cras sed lectus nisl.
 Integer posuere at metus quis pretium. Suspendisse leo odio, dapibus non metus ac, mattis ornare nisl. Nunc ut turpis sit amet mi elementum laoreet. Curabitur maximus nunc in purus blandit consequat. Quisque mollis elit efficitur magna auctor, a rutrum sem pulvinar. Nunc id luctus.`;
-
+*/
 const typeContainer = document.querySelector('.typing-field');
 const brTag = document.createElement('br');
 
 const timerField = document.querySelector('.timer');
 const typingSpeedField = document.querySelector('.cpm');
+const mistakesField = document.querySelector('.mistakes');
 
 const timer = {
   interval: 1000,
@@ -55,6 +56,7 @@ formatTimeDifference = (startTime, endTime, withMs = false) => {
 };
 
 let textPosition = 0;
+let mistakes = 0;
 
 let startTypingTime;
 let endTypingTime;
@@ -96,6 +98,9 @@ window.addEventListener('keydown', (e) => {
     const letter = document.querySelector(
       `.letter[data-order='${textPosition}']`
     );
+
+    // the spacebar key initiate scroll event
+    // this condition need to prevent it 
     if (e.key === ' ') {
       console.log('Space has been pressed.');
       e.preventDefault();
@@ -138,6 +143,7 @@ window.addEventListener('keydown', (e) => {
           Math.max(0, offsetCursorForScroll),
           offsetCursorForScroll
         );
+        // end of autoscroll
       }
       letter.classList.add('right');
       console.log(textPosition);
@@ -161,6 +167,7 @@ window.addEventListener('keydown', (e) => {
       }
     } else {
       letter.classList.add('mistake');
+      mistakesField.innerText = ++mistakes;
     }
   }
 });
